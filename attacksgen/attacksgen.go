5f8758f3c40d33e8fd43df82f9e5c5abe9a50bb1
Conrado Jordan <conrado.jordan@gmail.com>
2024-04-06 15:42:39 -0300

    Add GenerateBishopAttacks()

--- attacksgen/attacksgen.go
+++ attacksgen/attacksgen.go
@@ -15,3 +15,27 @@ func GenerateRookAttacks() [64]uint64 {
 	}
 	return result
 }
+
+func GenerateBishopAttacks() [64]uint64 {
+	var result [64]uint64
+	var magicNumber uint64
+	var current uint64 = 0x8040_2010_0804_0200
+	var magicNumber2 uint64 = 0x0042_2418_1824_4280
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			result[i*8+j] = current
+			magicNumber = 0x0000_0000_0000_0001 << (((i+j)*8)%56 + 8) & 0xffff_ffff_ffff_ffff
+			magicNumber ^= 0x0000_0000_0000_0001 << ((64 + 8*i - 8*j) % 72) & 0xffff_ffff_ffff_ffff
+
+			if j == 7 {
+				magicNumber2 = (magicNumber2 << 8) & 0xffff_ffff_ffff_ffff
+				magicNumber2 = magicNumber2 ^ ((0x0000_0000_0000_0100 >> i) & 0x0000_0000_0000_00ff)
+				magicNumber2 = magicNumber2 ^ ((0x0000_0000_0000_0002 << i) & 0x0000_0000_0000_00ff)
+				magicNumber = magicNumber2
+			}
+			current = ((current & 0x7fff_ffff_ffff_ffff) << 1) ^ magicNumber
+		}
+	}
+	return result
+}
