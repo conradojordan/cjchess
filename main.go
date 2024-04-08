c9d542d6b6ef5685a9c98a280032c459ede5881e
Conrado Jordan <conrado.jordan@gmail.com>
2024-04-07 22:27:37 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/conradojordan/cjchess/attacksgen"
 	"github.com/conradojordan/cjchess/board"
 )
 
@@ -13,10 +16,16 @@ func main() {
 	// board.PrintBitboard(b.Wpawns)
 	// board.PrintBitboard(b.Brooks)
 
-	// a := attacksgen.GenerateKnightAttacks()
+	a := attacksgen.GenerateBPawnAttacks()
 
-	// for i := 0; i < 64; i += 4 {
-	// 	fmt.Printf("	0x%016x, 0x%016x, 0x%016x, 0x%016x,\n", a[i], a[i+1], a[i+2], a[i+3])
+	// for _, v := range a {
+	// 	board.PrintBitboard(v)
+	// 	fmt.Scanln()
 	// }
 
+	fmt.Println("var BPawnAttacks [64]uint64 = [64]uint64{")
+	for i := 0; i < 64; i += 4 {
+		fmt.Printf("	0x%016x, 0x%016x, 0x%016x, 0x%016x,\n", a[i], a[i+1], a[i+2], a[i+3])
+	}
+	fmt.Println("}")
 }
