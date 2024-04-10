a2743e7a162c6da8f520a34bed741ebcf98a7c14
Conrado Jordan <conrado.jordan@gmail.com>
2024-04-09 21:40:15 -0300

    Finish attack generation functions

--- attacksgen/attacksgen.go
+++ attacksgen/attacksgen.go
@@ -16,30 +16,6 @@ func GenerateRookAttacks() [64]uint64 {
 	return result
 }
 
-func GenerateBishopAttacks() [64]uint64 {
-	var result [64]uint64
-	var magicNumber uint64
-	var current uint64 = 0x8040_2010_0804_0200
-	var magicNumber2 uint64 = 0x0042_2418_1824_4280
-
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			result[i*8+j] = current
-			magicNumber = 1 << (((i+j)*8)%56 + 8) & 0xffff_ffff_ffff_ffff
-			magicNumber ^= 1 << ((64 + 8*i - 8*j) % 72) & 0xffff_ffff_ffff_ffff
-
-			if j == 7 {
-				magicNumber2 = (magicNumber2 << 8) & 0xffff_ffff_ffff_ffff
-				magicNumber2 = magicNumber2 ^ ((0x100 >> i) & 0xff)
-				magicNumber2 = magicNumber2 ^ ((0x2 << i) & 0xff)
-				magicNumber = magicNumber2
-			}
-			current = ((current & 0x7fff_ffff_ffff_ffff) << 1) ^ magicNumber
-		}
-	}
-	return result
-}
-
 func GenerateKnightAttacks() [64]uint64 {
 	// - - - - - - - -
 	// - a - b - - - -
@@ -111,6 +87,30 @@ func GenerateKnightAttacks() [64]uint64 {
 	return result
 }
 
+func GenerateBishopAttacks() [64]uint64 {
+	var result [64]uint64
+	var magicNumber uint64
+	var current uint64 = 0x8040_2010_0804_0200
+	var magicNumber2 uint64 = 0x0042_2418_1824_4280
+
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			result[i*8+j] = current
+			magicNumber = 1 << (((i+j)*8)%56 + 8) & 0xffff_ffff_ffff_ffff
+			magicNumber ^= 1 << ((64 + 8*i - 8*j) % 72) & 0xffff_ffff_ffff_ffff
+
+			if j == 7 {
+				magicNumber2 = (magicNumber2 << 8) & 0xffff_ffff_ffff_ffff
+				magicNumber2 = magicNumber2 ^ ((0x100 >> i) & 0xff)
+				magicNumber2 = magicNumber2 ^ ((0x2 << i) & 0xff)
+				magicNumber = magicNumber2
+			}
+			current = ((current & 0x7fff_ffff_ffff_ffff) << 1) ^ magicNumber
+		}
+	}
+	return result
+}
+
 func GenerateQueenAttacks() [64]uint64 {
 	var result [64]uint64
 
@@ -119,6 +119,33 @@ func GenerateQueenAttacks() [64]uint64 {
 
 	for i := 0; i < 64; i++ {
 		result[i] = r[i] ^ b[i]
+	}
+
+	return result
+}
+
+func GenerateKingAttacks() [64]uint64 {
+	var result [64]uint64
+	var a, b uint64
+
+	for i := 0; i < 64; i++ {
+		a = 7 << (7 + i)
+		b = 0xff << ((i/8)*8 + 8)
+		result[i] ^= a & b
+	}
+
+	for i := 0; i < 63; i++ {
+		a = ((5 << 61) >> (62 - i))
+		b = 0xff << ((i / 8) * 8)
+		result[i] ^= a & b
+	}
+	result[63] ^= 1 << 62
+
+	for i := 0; i < 64; i++ {
+		a = (7 << 54)
+		a = a >> (63 - i)
+		b = (0xff << ((i / 8) * 8)) >> 8
+		result[i] ^= a & b
 	}
 
 	return result
@@ -172,4 +199,32 @@ func GenerateBPawnAttacks() [64]uint64 {
 	}
 
 	return results
+}
+
+func GenerateWPawnMoves() [64]uint64 {
+	var result [64]uint64
+
+	for i := 0; i < 56; i++ {
+		result[i] = 1 << (8 + i)
+	}
+
+	for i := 8; i < 16; i++ {
+		result[i] = result[i] ^ (1 << (16 + i))
+	}
+
+	return result
+}
+
+func GenerateBPawnMoves() [64]uint64 {
+	var result [64]uint64
+
+	for i := 8; i < 64; i++ {
+		result[i] = 1 << (i - 8)
+	}
+
+	for i := 48; i < 56; i++ {
+		result[i] = result[i] ^ (1 << (i - 16))
+	}
+
+	return result
 }
